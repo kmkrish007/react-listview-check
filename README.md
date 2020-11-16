9b2173b59b837f9bf1acdbfd614da10f2b94f59e
kmkrish007 <muthu555krish@gmail.com>
2020-11-16 10:50:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-listview-check
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-listview-check)
---
+++ index.js
@@ -0,0 +1,35 @@
+import { render } from "react-dom";
+import "./index.css";
+/**
+ * ListView CheckList Sample
+ */
+import * as React from "react";
+import { ListViewComponent } from "@syncfusion/ej2-react-lists";
+import { SampleBase } from "./sample-base";
+
+import { flatList, groupData } from "./listData";
+export class Checklist extends SampleBase {
+  constructor() {
+    super(...arguments);
+    //Map the appropriate columns to fields property
+    this.fields = { groupBy: "category" };
+  }
+  render() {
+    return (
+      <div className="control-pane">
+        <div className="control-section">
+          <div id="flat-list">
+            <h4>Flat List</h4>
+            <ListViewComponent
+              id="sample-list-flat"
+              dataSource={flatList}
+              showCheckBox={true}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<Checklist />, document.getElementById("sample"));
